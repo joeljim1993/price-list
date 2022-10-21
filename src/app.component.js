9fe19e8b7006418ca4b15149369a359aa0f07d05
Josue Rojas <josuerojas161296@gmail.com>
2022-10-21 07:26:33 -0400

    inicio de proyecto

---
+++ src/app.component.js
@@ -0,0 +1,16 @@
+import { html, css, LitElement } from "lit";
+
+export class AppComponent extends LitElement {
+  static properties = {};
+
+  constructor() {
+    super();
+  }
+  static styles = css`
+   
+  `;
+  render() {
+   
+  }
+}
+customElements.define("app-root", AppComponent);
