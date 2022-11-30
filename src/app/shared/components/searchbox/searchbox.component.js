df0d2fb2149828b0ef1a278a0afe4b90454f93ff
joeljim <joel_jimenez@live.com>
2022-11-30 11:09:15 -0400

    #8 search-box

--- src/app/shared/components/searchbox/searchbox.component.js
+++ src/app/shared/components/searchbox/searchbox.component.js
@@ -1,20 +1,30 @@
 import { html, css, LitElement } from "lit";
-import { fromEvent } from "rxjs";
-import { debounceTime } from "rxjs/internal/operators/debounceTime";
-import { map } from "rxjs/internal/operators/map";
-import { tap } from "rxjs/internal/operators/tap";
-import { filter } from "rxjs/internal/operators/filter";
+import { Router } from "@vaadin/router";
+import {
+  fromEventPattern,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  takeUntil,
+  tap,
+  of,
+  Subject,
+  fromEvent,
+  take,
+  last,
+} from "rxjs";
 
-import { switchMap } from "rxjs/internal/operators/switchMap";
-import { service } from "../../../core/services/service";
 export class SearchBoxComponent extends LitElement {
-  static styles = css`
-    .search-button{
-      width:300px;
-      height:25px;
-      border-radius:5px;
-      background: #FAF9F9;
 
+  click$ = fromEvent(document, "keyup");
+
+  static styles = css`
+    .search-button {
+      width: 300px;
+      height: 25px;
+      border-radius: 5px;
+      background: #faf9f9;
     }
   `;
 
@@ -24,18 +34,32 @@ export class SearchBoxComponent extends LitElement {
 
   render() {
     return html`
-      <div class="box-container">
-        <input
-          type="text"
-          placeholder="Buscar Producto"
-          class="search-button"
-        />
+      <div class="input">
+        <input placeholder="introduce el producto " class="search-button" />
       </div>
     `;
   }
 
- 
-
+  firstUpdated() {
+    let query = "";
+    const result = this.click$.pipe(
+      map((info) => info.key),
+      distinctUntilChanged(),
+      filter(
+        (pressedKey) => pressedKey.match(/[a-z]/i) && pressedKey.length == 1
+      ),
+      tap((data) => (query = query + data)),
+      tap(() => console.log("query desde la consola ", query)),
+      tap(() => this.filterData(query)),
+      debounceTime(2000),
+      tap(() => (query = ""))
+    );
+    result.subscribe();
+  }
+ // funcion que debe ser creada en el servicio 
+  filterData(data) {
+    console.log("estos son los datos introducidos en el filter ", data);
+  }
 }
 
 customElements.define("searchbox-component", SearchBoxComponent);
