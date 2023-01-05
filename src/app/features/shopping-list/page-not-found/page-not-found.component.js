e068ea96e61693bad6ab37c90fbe23b759a43779
Josue Rojas <josuerojas161296@gmail.com>
2023-01-05 15:52:19 -0400

    Cambios 05/01/23

--- src/app/features/shopping-list/page-not-found/page-not-found.component.js
+++ src/app/features/shopping-list/page-not-found/page-not-found.component.js
@@ -1,6 +1,71 @@
 import { LitElement, html, css } from 'lit';
+import { Router } from '@vaadin/router';
 
 export class PageNotFound extends LitElement {
+
+    static styles = css`
+        * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        @keyframes rotateIn {
+            from {
+            transform: rotate( 0deg ) scale(0.2);
+            opacity: 0;
+            }
+            to {
+            transform: rotate( 360deg ) scale(1);
+            opacity: 1;
+            }
+        }
+
+        .container {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        picture {
+            animation: rotateIn 0.5s ease-out;
+            margin-bottom: 45px;
+        }
+
+        h1 {
+            font-weight: 700;
+            font-size: 48px;
+        }
+
+        h2 {
+            font-weight: 400;
+            font-size: 48px;
+        }
+
+        button {
+            width: 177px;
+            height: 39px;
+            background: #F4A534;
+            border: none;
+            border-radius: 10px;
+            line-height: 21px;
+            text-align: center;
+            font-size: 24px;
+            font-weight: 500;
+            height: 50px;
+            width: 250px;
+            cursor: pointer;
+            margin-top: 70px;
+        }
+
+        button:hover {
+            background: #ee9314;
+        }
+    `
 
     static properties = {
     }
@@ -11,9 +76,19 @@ export class PageNotFound extends LitElement {
 
     render() {
         return html`
-            <h1>Error 404</h1>
-            <h2>Pagina no encontrada</h2>
+            <div class="container">
+                <picture>
+                    <img src='/src/assets/images/zanahoria.svg'>
+                </picture>
+                <h1>PAGE NOT FOUND</h1>
+                <h2>404</h2>
+                <button @click="${this.goBack}">Volver</button>
+            </div>
         `;
+    }
+
+    goBack() {
+        Router.go('/browse/');
     }
 }
 customElements.define('page-not-found', PageNotFound);
