85dbf14af627b884487a2fdce05589179a7f94d5
Alejandro Sanchez <alejo.upp@gmail.com>
2023-03-17 11:35:31 -0400

    Vista de favoritos añadida

--- src/app/features/favorites/favorites-browse.component.js
+++ src/app/features/favorites/favorites-browse.component.js
@@ -1,40 +1,54 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 import { Router } from "@vaadin/router";
 
-import { favoriteService } from '../../core/services/favorite.service';
+import { favoriteService } from "../../core/services/favorite.service";
+import './favorites-browse.styles.css';
 
- 
 export class FavoritesBrowse extends LitElement {
+  static properties = {};
 
-    static properties = {
-         };
+  constructor() {
+    super();
+    this.favoriteSrv = favoriteService;
+    this.favoriteList = this.favoriteSrv.getFavorites();
+    console.log("this.favoriteList",this.favoriteList);
+  }
 
-    constructor(){
-      
-        super();
-        this.favoriteSrv = favoriteService;
-        this.favoriteList = this.favoriteSrv.getFavorites();
-    }
+  refresh(event) {
+    const product = event.detail.product;
+    this.favoriteSrv.newFavorite$.next(product);
+    this.favoriteList = this.favoriteList.filter(
+      (productFavorite) => productFavorite.id !== product.id
+    );
+    this.requestUpdate();
+  }
 
-    refresh(event) {
-        const product = event.detail.product;
-        this.favoriteSrv.newFavorite$.next(product);
-        this.favoriteList = this.favoriteList.filter(productFavorite => productFavorite.id !== product.id);
-        this.requestUpdate();
-    }
+  render() {
+    return html`
+      <div class="favorite-container">
+        
+        <div class="options">
+          <i class="material-icons" @click=${this.goBack}>arrow_back</i>
+          <h1>Mis favoritos</h1>
+        </div>
 
-    render() {
-      return html`
-        <div class="favorite-container">
-          <h1>mostrando lementos guardados en favoritos</h1>
-          ${this.favoriteList.map((element) => {
-            console.log(element)
-            return html`
-            <h3>${element.name}</h3>
-            `;
+        <div class="products">
+          ${this.favoriteList.map((product) => {
+            return html` <product-card .product="${product}"></product-card> `;
           })}
         </div>
-      `;
-    }
+
+      </div>
+    `;
+  }
+
+  goBack(){
+    Router.go("/browse/")
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
 }
-customElements.define('favorites-browse', FavoritesBrowse);
+customElements.define("favorites-browse", FavoritesBrowse);
