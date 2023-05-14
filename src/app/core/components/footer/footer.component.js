ac3dfe113dd396c43ce01e06fec24579e9af4b8a
joel <joel_jimenez@live.com>
2023-05-14 13:30:06 -0400

    cambios footer

--- src/app/core/components/footer/footer.component.js
+++ src/app/core/components/footer/footer.component.js
@@ -2,8 +2,6 @@ import { html, css, LitElement } from "lit";
 import { tap } from "rxjs";
 import "./footer.styles.css";
 
-
-
 export class FooterComponent extends LitElement {
   constructor() {
     super();
@@ -11,12 +9,10 @@ export class FooterComponent extends LitElement {
 
   firstUpdated() {}
 
-
-
-
   render() {
     return html`
       <div class="footer-container ">
+
         <div class="group-one">
           <div class="box" id="box-img">
             <figure>
@@ -30,43 +26,64 @@ export class FooterComponent extends LitElement {
             </figure>
           </div>
           <div class="box">
-            <h2 class="title-info-cecosesola">SOBRE NOSOTROS</h2>
-            <p class="text-info-cecosesola">Somos una RED DE INTEGRACION con mas de 50 organizaciones comunitarias </p>
-            <a class="link-page-cecosesola" href="https://cecosesola.org/" target="_blank"
+            <!-- <h2 class="title-info-cecosesola">SOBRE NOSOTROS</h2> -->
+            <p class="text-info-cecosesola">
+              Somos una RED DE INTEGRACION con mas de 50 organizaciones
+              comunitarias
+            </p>
+            <a
+              class="link-page-cecosesola"
+              href="https://cecosesola.org/"
+              target="_blank"
               >¡Visita nuestra pagina y Conocenos!
             </a>
-            
           </div>
 
           <div class="red-social">
-            <h6 class="red-social-title" id="red-social-title">SIGUENOS:</h6>
+
+            <h6 class="red-social-title" id="red-social-title"> REDES SOCIALES:</h6>
             <div class="red-social-icons">
               <a
+                id="social-icons"
                 href="https://www.facebook.com/RedCecosesola/"
                 target="_blank"
                 class="fa fa-facebook"
               ></a>
               <a
+                id="social-icons"
                 href="https://www.instagram.com/redcecosesola/"
                 target="_blank"
                 class="fa fa-instagram"
               ></a>
               <a
+                id="social-icons"
                 href="https://twitter.com/redcecosesola"
                 target="_blank"
                 class="fa fa-twitter"
               ></a>
               <a
+                id="social-icons"
                 href="https://www.youtube.com/channel/UC4Z6igtx966wHbfeumY9UwA"
                 target="_blank"
                 class="fa fa-youtube"
               ></a>
+
+              <figure>
+              <a id="form" href="https://forms.gle/APaFDevGbqwiF8ts9" target="_blank"><p>Tu Opinion es importante , Evaluanos </p>
+              <i 
+                    class="material-icons" 
+                    id="icon-form"
+                >app_registration</i>
+            </figure>
             </div>
+           
+            
           </div>
-          <div>
-            <a>Tu Opinion es importante , Evaluanos   </a>
-             
-          </div>
+          <div class="form">
+            
+            </div>
+            
+          
         </div>
         <div class="group-two">
           <small
